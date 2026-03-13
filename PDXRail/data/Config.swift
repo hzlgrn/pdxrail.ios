a35bb8a0e4d18727a50e3d0841e34f1a87bea70b
Berto <berto@hzlgrn.com>
2026-03-13 12:04:52 -0700

    Key handling?

--- PDXRail/data/Config.swift
+++ PDXRail/data/Config.swift
@@ -1,4 +1,6 @@
+import Foundation
+
 enum Config {
-    static let apiBaseURL = ""
-    static let apiKey     = ""
+    static let apiBaseURL = Bundle.main.infoDictionary?["RAIL_SYSTEM_URL"] as? String ?? ""
+    static let apiKey     = Bundle.main.infoDictionary?["RAIL_SYSTEM_KEY"] as? String ?? ""
 }
